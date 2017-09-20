5189c17bd6115073ca22ae0a721c45f49b5351b7
Saikiran Ambati <31777778+Ambati1304@users.noreply.github.com>
2017-09-20 14:22:00 -0500

    Update spdump.py

--- spdump.py
+++ spdump.py
@@ -8,6 +8,7 @@ cur.execute('''SELECT COUNT(from_id) AS inbound, old_rank, new_rank, id, url
      WHERE html IS NOT NULL
      GROUP BY id ORDER BY inbound DESC''')
 
+
 count = 0
 for row in cur :
     if count < 50 : print(row)
